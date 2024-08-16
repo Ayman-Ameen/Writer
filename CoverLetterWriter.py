faba47425135e104fc148f817781111c67269215
Ayman <aiman2017@gmail.com>
2024-08-16 13:26:48 +0200

    Adding Coverletter and req

---
+++ CoverLetterWriter.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from metagpt.actions import Action
+from metagpt.logs import logger
+from metagpt.roles import Role
+from metagpt.schema import Message
+from metagpt.team import Team
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
+
+'''
+To enable a large language model (LLM) to perform a multi-agent task such as writing a cover letter, several specialized agents or components can be utilized
+'''
+
